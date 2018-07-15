3e7343f7617fa2c8776e8324a8052ec6bd1fbed8
luciapuccini <puccinilucia@gmail.com>
2018-07-15 15:17:46 -0300

    deploying

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
-
+const path = require('path')
 const items = require("./routes/api/items");
 // middleware for body parser
 app.use(bodyParser.json());
 
 //DB config
-const db = require("./config/keys").mongoURI;
+const db = require("./config/keys")
+  .mongoURI;
 
 //connect to mongo
 mongoose
@@ -17,5 +18,15 @@ mongoose
   .catch(e => console.log(e));
 //use routes
 app.use("/api/items", items);
+
+//SErv static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+
+}
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
