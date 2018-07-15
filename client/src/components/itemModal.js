b861a3c897892b6629be1d14606f783670ee9b62
luciapuccini <puccinilucia@gmail.com>
2018-07-15 14:41:41 -0300

    end MERN app

--- client/src/components/itemModal.js
+++ client/src/components/itemModal.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Button, Modal, modalHeader, ModalBody, Form, FormGroup, Label, Input, ModalHeader } from 'reactstrap';
 import { connect } from 'react-redux';
 import { addItem } from '../actions/itemActions';
-import uuid from 'uuid'
 
 class ItemModal extends Component {
   state = {
@@ -22,7 +21,6 @@ class ItemModal extends Component {
   onSubmit = (event) => {
     event.preventDefault();
     const newItem = {
-      id: uuid(),
       name: this.state.name
     }
     this.props.addItem(newItem)
